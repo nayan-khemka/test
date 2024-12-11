e810970405a742935a926a383f67b3e4a66ec0d1
Nayan Khemka <72384862+nayan-khemka@users.noreply.github.com>
2024-12-11 09:01:31 +0530

    Update .py

--- .py
+++ .py
@@ -19,25 +19,30 @@ def scrape_table():
     url = "https://www.gasnetworks.ie/corporate/gas-regulation/transparency-and-publicat/dashboard-reporting/balancing-actions-and-prices/imbalance-prices/"
     driver.get(url)
 
-    # Wait for the table to be present
     table = driver.find_element(By.CSS_SELECTOR, 'table')  # Adjust the selector as needed
     rows = table.find_elements(By.TAG_NAME, 'tr')
-
+    # print(table)
+    # print("\n")
+    # print("\n")
+    # print(rows)
     data = []
     for row in rows:
         cols = row.find_elements(By.TAG_NAME, 'td')
+        if cols == []:
+            cols = row.find_elements(By.TAG_NAME, 'th')
         data.append([col.text for col in cols])
-    
+    # print(data)
     driver.quit()
     
     # Convert the data to a DataFrame
     df = pd.DataFrame(data[1:], columns=data[0])  # Assuming first row contains column names
     df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y')
-    for col in df.columns[1:]:
-        df[col] = pd.to_numeric(df[col], errors='coerce')
+    # for col in df.columns[1:]:
+    #     df[col] = pd.to_numeric(df[col], errors='coerce')
+    print(df)
     return df
 
-# Example usage
+
 table_data = scrape_table()
 print(table_data)
 
@@ -93,5 +98,4 @@ def save_table_to_db(data):
 
 table_data = scrape_table()
 save_table_to_db(table_data)
-
 
